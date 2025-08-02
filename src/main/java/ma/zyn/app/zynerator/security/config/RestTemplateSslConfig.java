b4568a5807320a7e0eb322b64b626e53f852bfd1
Mohammed Ezzaim <mohammedezzaim00@gmail.com>
2025-08-02 13:45:28 +0100

    test deploy with default rout

--- src/main/java/ma/zyn/app/zynerator/security/config/RestTemplateSslConfig.java
+++ src/main/java/ma/zyn/app/zynerator/security/config/RestTemplateSslConfig.java
@@ -21,33 +21,33 @@ public class RestTemplateSslConfig {
 
     @Bean
     public RestTemplate restTemplate() throws Exception {
-        char[] keystorePassword = "zynkey".toCharArray();
-
-        KeyStore keyStore = KeyStore.getInstance("PKCS12");
-        keyStore.load(new FileSystemResource("zyn/ssl/localhost/keystore.p12").getInputStream(), keystorePassword);
-
-        SSLContext sslContext = SSLContextBuilder.create()
-                .loadKeyMaterial(keyStore, keystorePassword)
-                .loadTrustMaterial(keyStore, (chain, authType) -> true)
-                .build();
-
-        PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
-                .setSSLSocketFactory(
-                        SSLConnectionSocketFactoryBuilder.create()
-                                .setSslContext(sslContext)
-                                .build()
-                )
-                .build();
-
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setConnectionManager(connectionManager)
-                .build();
-
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-
-
-        return new RestTemplate(requestFactory);
-        //return new RestTemplate();
+//        char[] keystorePassword = "zynkey".toCharArray();
+//
+//        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+//        keyStore.load(new FileSystemResource("zyn/ssl/localhost/keystore.p12").getInputStream(), keystorePassword);
+//
+//        SSLContext sslContext = SSLContextBuilder.create()
+//                .loadKeyMaterial(keyStore, keystorePassword)
+//                .loadTrustMaterial(keyStore, (chain, authType) -> true)
+//                .build();
+//
+//        PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+//                .setSSLSocketFactory(
+//                        SSLConnectionSocketFactoryBuilder.create()
+//                                .setSslContext(sslContext)
+//                                .build()
+//                )
+//                .build();
+//
+//        CloseableHttpClient httpClient = HttpClients.custom()
+//                .setConnectionManager(connectionManager)
+//                .build();
+//
+//        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+//
+//
+//        return new RestTemplate(requestFactory);
+        return new RestTemplate();
     }
 
 }
