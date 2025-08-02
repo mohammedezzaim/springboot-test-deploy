414bcf9ff4d8d9fd7086dfb2ff64ad831de6fb4c
Mohammed Ezzaim <mohammedezzaim00@gmail.com>
2025-08-02 13:37:06 +0100

    test deploy with default rout

---
+++ src/main/java/ma/zyn/app/ws/facade/admin/HealthController.java
@@ -0,0 +1,13 @@
+package ma.zyn.app.ws.facade.admin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Railway!";
+    }
+}
